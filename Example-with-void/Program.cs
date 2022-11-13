85aa1a5e9240f0befdbd5ab45304cc57de799aea
ladynazgool <ladynazgool@gmail.com>
2022-11-13 02:20:35 +0100

    Example with void finished

--- Example-with-void/Program.cs
+++ Example-with-void/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        Console.Write($"Enter {i + 1} element of array: ");
+        array[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+}
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for(int i=0;i<length;i++)
+    {
+        Console.Write($"Enter {i + 1} element of array: ");
+        array[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+int[] createdArray = CreateArray(5);
+PrintArray(createdArray);
+Console.WriteLine();
+int[] array = new int[5];
+FillArray(array);
+PrintArray(array);
