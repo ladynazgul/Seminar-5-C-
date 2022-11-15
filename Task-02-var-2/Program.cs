50e46b5f6b8f94bfa45e35d6879a0e20a256a116
ladynazgool <ladynazgool@gmail.com>
2022-11-15 14:03:33 +0100

    Task-02 variant 2 added

---
+++ Task-02-var-2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача про перевод из десятичной в двоичную систему. Решение Наты
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    int number = int.Parse(Console.ReadLine() ?? "0"); // Этот метод для получения десятичного числа
+    return number;
+}
+
+int[] ConvertToBinary(int num)
+{
+    int num2 = num;
+    int size = 0;
+    while (num > 0)
+    {
+        num = num / 2;
+        size++;
+    }
+
+    int[] binary = new int[size];
+    for (int i = size - 1; i >= 0; i--)
+    {
+        binary[i] = num2 % 2;
+        num2 = num2 / 2;
+    }
+    return binary;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i]);
+    }
+}
+
+PrintArray(ConvertToBinary(GetNumber()));
+
+// Решение препода
+
