bcb1018f196005eaa026fddab72335604ee6bf10
ladynazgool <ladynazgool@gmail.com>
2022-12-02 09:01:38 +0100

    simple solution for task 42 added

--- Task-1_42/Program.cs
+++ Task-1_42/Program.cs
@@ -1,44 +1,61 @@
-﻿// Задача 42: Определить сколько чисел больше 0 введено с клавиатуры
-// Решение с методами
+﻿// // Задача 42: Определить сколько чисел больше 0 введено с клавиатуры
+// // Решение с методами
 
-int GetArrayLength()
+// int GetArrayLength()
+// {
+//     Console.WriteLine("Укажите количество чисел, которые будут вводиться с клавиатуры: ");
+//     int number = int.Parse(Console.ReadLine() ?? "0");
+//     return number;
+// }
+
+// void FillArray(int[] Array)
+// {
+//     for(int i = 0; i < Array.Length; i++)
+//     {
+//         Console.WriteLine($"Введите {i + 1} число: ");
+//         Array[i] = int.Parse(Console.ReadLine() ?? "0");
+//     }
+// }
+
+// void ShowNumbers(int[] Array)
+// {
+//     for(int i = 0; i < Array.Length; i++)
+//     Console.Write($"{Array[i]} ");
+// }
+
+// int CheckArray(int[] Array)
+// {
+//     int result = 0;
+//     for(int i = 0; i < Array.Length; i++)
+//     {
+//         if (Array[i] > 0)
+//             result++;
+//     }
+//     return result;
+// }
+
+// int Length = GetArrayLength();
+// int[] Numbers = new int[Length];
+// FillArray(Numbers);
+// ShowNumbers(Numbers);
+// int count = CheckArray(Numbers);
+// Console.WriteLine();
+
+// Console.WriteLine($"Данный массив содержит {count} чисел больше 0");
+
+// Решение без методов и массивов
+
+int length = 10;
+int count = 0;
+
+while (length > 0)
 {
-    Console.WriteLine("Укажите количество чисел, которые будут вводиться с клавиатуры: ");
-    int number = int.Parse(Console.ReadLine() ?? "0");
-    return number;
+    Console.Write("Введите число: ");
+    string num = Console.ReadLine();
+    int number = int.Parse(num);
+    if (number > 0)
+        count++;
+        length--;
 }
 
-void FillArray(int[] Array)
-{
-    for(int i = 0; i < Array.Length; i++)
-    {
-        Console.WriteLine($"Введите {i + 1} число: ");
-        Array[i] = int.Parse(Console.ReadLine() ?? "0");
-    }
-}
-
-void ShowNumbers(int[] Array)
-{
-    for(int i = 0; i < Array.Length; i++)
-    Console.Write($"{Array[i]} ");
-}
-
-int CheckArray(int[] Array)
-{
-    int result = 0;
-    for(int i = 0; i < Array.Length; i++)
-    {
-        if (Array[i] > 0)
-            result++;
-    }
-    return result;
-}
-
-int Length = GetArrayLength();
-int[] Numbers = new int[Length];
-FillArray(Numbers);
-ShowNumbers(Numbers);
-int count = CheckArray(Numbers);
-Console.WriteLine();
-
-Console.WriteLine($"Данный массив содержит {count} чисел больше 0");
+Console.WriteLine($"Количество чисел больше 0: {count}");
