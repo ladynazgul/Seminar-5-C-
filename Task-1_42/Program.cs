4d89b2a9ad0c85b0311d02b8e761e1ad105a2f0e
ladynazgool <ladynazgool@gmail.com>
2022-12-02 08:03:56 +0100

    Task 42 finished

---
+++ Task-1_42/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 42: Определить сколько чисел больше 0 введено с клавиатуры
+// Решение с методами
+
+int GetArrayLength()
+{
+    Console.WriteLine("Укажите количество чисел, которые будут вводиться с клавиатуры: ");
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void FillArray(int[] Array)
+{
+    for(int i = 0; i < Array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        Array[i] = int.Parse(Console.ReadLine() ?? "0");
+    }
+}
+
+void ShowNumbers(int[] Array)
+{
+    for(int i = 0; i < Array.Length; i++)
+    Console.Write($"{Array[i]} ");
+}
+
+int CheckArray(int[] Array)
+{
+    int result = 0;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i] > 0)
+            result++;
+    }
+    return result;
+}
+
+int Length = GetArrayLength();
+int[] Numbers = new int[Length];
+FillArray(Numbers);
+ShowNumbers(Numbers);
+int count = CheckArray(Numbers);
+Console.WriteLine();
+
+Console.WriteLine($"Данный массив содержит {count} чисел больше 0");
