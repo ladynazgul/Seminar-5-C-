a70434ec18241bc4c31a08cfb6ed824bf8d456df
ladynazgool <ladynazgool@gmail.com>
2022-12-03 01:19:27 +0100

    Task-2_44 changed and finished

--- Task-2_44/Program.cs
+++ Task-2_44/Program.cs
@@ -11,6 +11,16 @@ double b2 = double.Parse(Console.ReadLine() ?? "0");
 
 Console.WriteLine("Две прямые пересекутся в точке с одинаковыми координатами, поэтому правые части уравнений нужно приравнять");
 Console.WriteLine($"{k1}x + {b1} = {k2}x + {b2}");
-double x = (b2 - b1)/(k1 - k2);
-double y = k1*x + b1;
-Console.Write($"координаты точки пересечения: ({x}, {y})");
+
+if (k1 == k2 && b1 == b2)
+    Console.WriteLine(" Прямые совпадают ");
+  
+else if (k1 == k2 || b1 == b2)
+    Console.WriteLine(" Точки пересечения нет ");
+
+else
+{
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1*x + b1;
+    Console.Write($"координаты точки пересечения: ({x}, {y})");
+}
