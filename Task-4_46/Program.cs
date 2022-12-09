8c8cdd8c2bb36c7fa50024648e0a7af3f87857ab
ladynazgool <ladynazgool@gmail.com>
2022-12-09 14:00:21 +0100

    Task 46 finished

--- Task-4_46/Program.cs
+++ Task-4_46/Program.cs
@@ -1,27 +1,45 @@
 ﻿// 46. Написать программу масштабирования фигуры
+// Масштабирование равностороннего четырехугольника (Левый нижний угол (точка А) остается на месте)
 
-// Масштабирование равностороннего четырехугольника. (Левый нижний угол (точка А) остается на месте)
-
-void PrintArray(double[,] array)
+int[,] ScaleCoords(int[] a, int[] b, int[] c, int[] d, int k) // метод для пересчитывания координат, для всех точек, кроме a
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    int[,] coord = new int[4, 2];                              // для вывода всех координат записываем их в двумерный массив
+    for (int ii = 0; ii < 2; ii++)                             // индекс массива - координаты точки
     {
-       for (int j = 0; j < array.GetLength(1); j++)
-        if (j % 2 == 0) 
-            Console.Write($"( {array[i,j]} , ");
-        else
-            Console.Write($"{array[i,j]} )  "); 
+        for (int j = 0; j < 2; j++)                            // индекс столбцов, индексы строк задаем ниже вручную
+        {
+            if (ii == j)                                       // индекс столбца равен индексу координаты точки
+            {
+                coord[0, j] = a[ii];                           // здесь задаем новые координаты с поправкой на неподвижную точку a
+                coord[1, j] = (b[ii] - a[ii]) * k + a[ii];
+                coord[2, j] = (c[ii] - a[ii]) * k + a[ii];
+                coord[3, j] = (d[ii] - a[ii]) * k + a[ii]; 
+            }
+        }
     }
+    return coord;
 }
-double[,] Scale(double[,] arr, double k)
+
+void PrintCoords(int[,] arr, char[] point)
 {
-    double[,] NewArray = new double[arr.GetLength(0), arr.GetLength(1)];
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-       for (int j = 0; j < arr.GetLength(1); j++)
-       {
-           NewArray[i,j] = arr[i,j] * k;
-       }
+        Console.Write($"{point[i]} ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write($"({arr[i, j]}, ");
+            else Console.WriteLine($"{arr[i, j]})");
+        }
     }
-    return NewArray;
 }
+
+int k = 2;
+int[] pointA = { 3, 2 };
+int[] pointB = { 2, 5 };
+int[] pointC = { 7, 5 };
+int[] pointD = { 8, 2 };
+char[] pointName = { 'A', 'B', 'C', 'D' };
+
+Console.WriteLine();
+int[,] newCoords = ScaleCoords(pointA, pointB, pointC, pointD, k);
+PrintCoords(newCoords, pointName);
