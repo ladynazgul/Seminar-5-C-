1ad56af01bfe5a3ba7ef67a607cb6e758eec67b1
ladynazgool <ladynazgool@gmail.com>
2023-01-17 14:20:13 +0100

    task 46 var 3 added

---
+++ Task-4_46-var3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу масштабирования фигуры. Способ решения, который показал препод
+
+int CountBlanks(string str) // Метод для подсчета пробелов в строке
+{
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+        if (str[i] == ' ')
+            count++;
+    return count;
+}
+
+var str = Console.ReadLine() ?? "0";
+int blanks = CountBlanks(str);
+
+var strings = new string[blanks + 1];
+int stringNumber = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    string point = "";
+    while(str[i] != ' ')
+        point += str[i];
+
+}
+
+
